3c3685b7002de63de10b1b99d2cfb52fdbc836ba
Tony Hirst <tony.hirst@gmail.com>
2020-06-18 18:17:40 +0100

    Update __init__.py

--- empinken/__init__.py
+++ empinken/__init__.py
@@ -7,7 +7,7 @@ def _jupyter_nbextension_paths():
     return [dict(section="notebook",
                  src="static",
                  dest="empinken",
-                 require="empinken/main")]
+                 require="empinken/index")]
 
 def load_jupyter_server_extension(nbapp):
     nbapp.log.info("empinken enabled!")

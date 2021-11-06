394a6f55985d818fa91d512f08b5b0b438fcd55b
Tony Hirst <tony.hirst@gmail.com>
2021-11-06 13:52:26 +0000

    Update version numbering; try to fix no-show of style

--- empinken/static/index.js
+++ empinken/static/index.js
@@ -172,6 +172,9 @@ define(['base/js/namespace', 'base/js/events', 'notebook/js/textcell', 'notebook
         };
     }
     var initialize = function () {
+
+        $.extend(true, params, Jupyter.notebook.config.data.empinken);
+        
         var layout_cell_color = function () {
             for (_typ of typs) {
                 var style = document.createElement("style");
@@ -224,19 +227,19 @@ define(['base/js/namespace', 'base/js/events', 'notebook/js/textcell', 'notebook
     }
 
     function load_jupyter_extension() {
-        return Jupyter.notebook.config.loaded
-            .then( function(){
-                $.extend(true, params, Jupyter.notebook.config.data.empinken); // update params
-            } )
-            .then(initialize);
+        //return Jupyter.notebook.config.loaded
+         //   .then( function(){
+         //       $.extend(true, params, Jupyter.notebook.config.data.empinken); // update params
+         //   } )
+         //   .then(initialize);
         //return Jupyter.notebook.config.loaded.then(initialize);
-        /*
+        
         if (Jupyter.notebook !== undefined && Jupyter.notebook._fully_loaded) {
             // notebook already loaded. Update directly
             initialize();
         }
         events.on("notebook_loaded.Notebook", initialize);
-        */
+        
     }
 
     return {

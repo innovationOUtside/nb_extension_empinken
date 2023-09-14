55956a7516ae9f06d3c39b9fc176f60fc1e44cdb
Tony Hirst <tony.hirst@gmail.com>
2023-09-14 16:01:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     url='https://github.com/innovationOUtside/nb_extension_empinken',
     include_package_data=True,
     install_requires=[
-        'notebook<7','jupyter_nbextensions_configurator'
+        'jupyter_nbextensions_configurator'
     ],
     data_files=[
         # like `jupyter nbextension install --sys-prefix`

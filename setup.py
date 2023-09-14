1f43db5ccb2bbf74293d52a2b3dbf9271f510006
Tony Hirst <tony.hirst@gmail.com>
2023-09-14 07:34:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import setuptools
+from os import path
+
+def get_long_description():
+    with open(
+        path.join(path.dirname(path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
 
 setuptools.setup(
     name="nb_extension_empinken",
     packages=['empinken'],
     #version=__version__, # Handled in setup.cfg and empinken/__init__.py
+    author="Tony Hirst",
+    author_email="tony.hirst@gmail.com",
+    description="Empinken cell background colouring for class and nbclassic Jupyter notebooks.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/innovationOUtside/nb_extension_empinken',
     include_package_data=True,
     install_requires=[
         'notebook', 'jupyter_nbextensions_configurator'
